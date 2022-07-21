83ce8274df6f94e895e1174264637fde798c88e6
bmcho <bmcho91@naver.com>
2022-07-21 11:28:54 +0900

    add: review,post,hashtag modeling finished  closed modeling (#2)
    모델링 남은 부분 완료

--- sns/apps/post/models.py
+++ sns/apps/post/models.py
@@ -1,11 +1,15 @@
 from django.db import models
-from sns.apps.core.models import TimeStampModel
+from apps.core.models import TimeStampModel
 
 
-# TODO: 아직 작성 할것 많이 남음
 # Create your models here.
 class Post(TimeStampModel):
     user = models.ForeignKey("user.User", on_delete=models.CASCADE)
     title = models.CharField(max_length=50, null=False)
     contents = models.TextField(null=False)
+
+class PostLike(TimeStampModel):
+    user = models.ForeignKey("user.User", on_delete=models.CASCADE)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+
 
