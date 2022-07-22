257d4f06ac91f7dcab936051d2d4e9117abe3dee
bmcho <57892199+bmcho@users.noreply.github.com>
2022-07-22 11:12:50 +0900

    Revert "[조병민] #4 - 유저가입, 유저 업데이트,로그인,로그아웃 기능 구현"

--- sns/apps/hashtag/models.py
+++ sns/apps/hashtag/models.py
@@ -1,10 +1,3 @@
-from statistics import mode
 from django.db import models
 
-from apps.core.models import TimeStampModel
-
 # Create your models here.
-class Hashtag(TimeStampModel):
-    hashtag_name = models.CharField(max_length=50, null=False)
-    post = models.ManyToManyField('post.Post', related_name="hashtags")
-
--- sns/sns/urls.py
+++ sns/sns/urls.py
@@ -14,27 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path, re_path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework.permissions import AllowAny
-
-schema_url_patterns = [path('users', include('apps.user.urls'))]
-
-schema_view_v1 = get_schema_view(
-    openapi.Info(
-        title='payhere',
-        default_version='v1',
-        description="payhere API",
-    ),
-    public=True,
-    permission_classes=[AllowAny],
-    patterns=schema_url_patterns,
-)
+from django.urls import path
 
 urlpatterns = [
-    path('users', include('apps.user.urls')),
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view_v1.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger$', schema_view_v1.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc$', schema_view_v1.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('admin/', admin.site.urls),
 ]
